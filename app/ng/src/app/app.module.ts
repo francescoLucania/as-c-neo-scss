a6de22608cf33979c38b4da05fef6bc9139246c1
francescolucania <akozlov@autospot.ru>
2020-08-28 15:40:30 +0300

    add buttons

--- app/ng/src/app/app.module.ts
+++ app/ng/src/app/app.module.ts
@@ -2,16 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {CommonAppModule} from "./module/common.module";
+// import {CommonAppModule} from "./module/common.module";
+import {PriceFormatterPipe} from "./module/pipes/price-formtter/price-formatter.pipe";
+import {CarOrderCard} from "./module/components/common/car-order-card/car-order-card";
 
 @NgModule({
   declarations: [
     AppComponent,
+    PriceFormatterPipe,
+    CarOrderCard
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonAppModule.forRoot()
+    // CommonAppModule.forRoot()
   ],
   providers: [
   ],
